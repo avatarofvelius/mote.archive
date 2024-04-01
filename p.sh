6340ee2be0404e29c08e28637c988c6b5e88b628
avatarofvelius <avatarofvelius@users.noreply.github.com>
2024-04-01 02:53:14 -0400

    general commit

--- p.sh
+++ p.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-git add . && git commit -m "general commit" && git push origin -f
+git add . && git commit -m "general commit" && git push origin 
+
+# -f : force push if online repo is outdated
